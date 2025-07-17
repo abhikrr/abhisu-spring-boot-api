52d71673967b4cf13ba0225a4143a64362f84654
abhikrr <abhik.raushan@gmail.com>
2025-07-17 10:10:46 +0530

    Add Kafka topic description endpoint and learning notes
    Added a new REST endpoint '/describe/{topicName}' in KafkaTopicController to fetch and display topic details such as partition count and replication factor. Also added KafkaLearning.txt with notes on Kafka concepts and use cases.

--- src/main/java/com/abhisu/abhisu_spring_boot_api/kafka/KafkaTopicController.java
+++ src/main/java/com/abhisu/abhisu_spring_boot_api/kafka/KafkaTopicController.java
@@ -1,14 +1,15 @@
 package com.abhisu.abhisu_spring_boot_api.kafka;
 
-import org.apache.kafka.clients.admin.AdminClient;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.common.TopicPartitionInfo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
@@ -22,6 +23,7 @@ public class KafkaTopicController {
 
     short replicationFactor = 1;
 
+    //$ bin/kafka-topics.sh --create --topic quickstart-events --bootstrap-server localhost:9092
     @GetMapping("/create-topic/{topicName}")
     public String createTopic(@PathVariable String topicName) {
 
@@ -56,5 +58,42 @@ public class KafkaTopicController {
             e.printStackTrace();
             return "An error occurred while fetching topics.";
         }
+    }
+
+    //$ bin/kafka-topics.sh --describe --topic quickstart-events --bootstrap-server localhost:9092
+    @GetMapping("/describe/{topicName}")
+    public String getKafkaTopic(@PathVariable String topicName) {
+
+        Properties config = new Properties();
+        config.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+
+        try (AdminClient adminClient = AdminClient.create(config)) {
+            DescribeTopicsResult result = adminClient.describeTopics(Collections.singleton(topicName));
+            Map<String, TopicDescription> descriptionMap = result.all().get();
+
+            TopicDescription description = descriptionMap.get(topicName);
+
+            System.out.println("📄 Topic: " + description.name());
+            System.out.println("Partitions: " + description.partitions().size());
+
+            List<TopicPartitionInfo> partitions = description.partitions();
+            for (TopicPartitionInfo partitionInfo : partitions) {
+                System.out.println("--------------------------------------------------");
+                System.out.println("Partition: " + partitionInfo.partition());
+                System.out.println("Leader: " + partitionInfo.leader());
+                System.out.println("Replicas: " + partitionInfo.replicas());
+                System.out.println("ISR: " + partitionInfo.isr());
+            }
+            return "Topic Id: " + description.topicId() +
+                    " Topic Name: " + description.partitions().size() +
+                    " Partition Count: " + description.partitions().size() +
+                    " Replication Factor: " + description.partitions().get(0).replicas().size();
+
+        } catch (ExecutionException e) {
+            System.err.println("Failed to describe topic: " + e.getCause().getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
